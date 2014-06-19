8e72a4aa75eddb793f2bcda18d833586a8f82cf4
ruthvik92 <ruthvik.nitc@gmail.com>
2014-06-19 16:19:49 +0530

    Update temprs485dht.py

--- RS485, PI,arduino codes/temprs485dht.py
+++ RS485, PI,arduino codes/temprs485dht.py
@@ -4,8 +4,8 @@ import sqlite3
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(7,GPIO.OUT)
+#GPIO.setup(12,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT) #port for direction slection on MAX485
 conn=sqlite3.connect('temp_table.db')
 c=conn.cursor()
 def get_temp_val():
@@ -14,14 +14,14 @@ def get_temp_val():
     ser.open()
     try:
         while True:
-            GPIO.output(7,1) 
+            GPIO.output(7,1) #put the max485 in transmission mode.
             time.sleep(.5)
             val=raw_input("enter something:") #send some value to ARDUINO
             ser.write(val)
             time.sleep(.5)
-            GPIO.output(7,0) 
+            GPIO.output(7,0) #put max485 in listening mode
             time.sleep(5)
-            temp=ser.read(ser.inWaiting())
+            temp=ser.read(ser.inWaiting()) # read the data
             print "received temp is :", temp
             #c.execute("""INSERT INTO sensor values(date('now'),time('now'),(?))""",(temp,))
             #conn.commit()
