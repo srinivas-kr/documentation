de9c12f499c1a5f0d0cf5707a187a5e976230770
ruthvik92 <ruthvik.nitc@gmail.com>
2014-06-19 16:26:14 +0530

    Update ardpixbee.py

--- digi,Pi,dht22/ardpixbee.py
+++ digi,Pi,dht22/ardpixbee.py
@@ -1,5 +1,7 @@
 #send quirey to arduino over xbee modules to receive temperature and humidity
 #without repeater
+#point to be noted is when repeater is not there master will not receive back 'A" or 'B' or 'C' or '
+# 'D' sent to slave  and slave will not receive back the values(temp and humidity) sent by it to master. 
 import serial
 import time
 import sys
@@ -7,7 +9,6 @@ ser=serial.Serial('/dev/ttyUSB0',9600)
 ser.close()
 ser.open()
 time.sleep(1)
-#ser.read()
 sys.stdout.flush()
 ser.flushInput()
 ser.flushOutput()
@@ -16,12 +17,10 @@ def send_data():
     c=raw_input("enter A for tmperature of house1 and B for humidity, enter C for temperature of house2 and D for humidity:")
     ser.write((c))
     print c
-
 def receive_data():
     while True:
         time.sleep(.5)
         a=ser.inWaiting()
-        
         b=ser.read(a)
         time.sleep(1)
         print 'receiving data is from:',c
